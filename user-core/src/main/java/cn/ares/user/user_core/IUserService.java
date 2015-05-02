89b86f5c485656f9a8782ef795800b5abdb966ed
ares <areswong813@foxmail.com>
2015-05-03 00:43:01 +0800

    测试

---
+++ user-core/src/main/java/cn/ares/user/user_core/IUserService.java
@@ -0,0 +1,5 @@
+package cn.ares.user.user_core;
+
+public interface IUserService {
+	void print ();
+}
---
+++ user-core/src/main/java/cn/ares/user/user_core/UserService.java
@@ -0,0 +1,9 @@
+package cn.ares.user.user_core;
+
+public class UserService implements IUserService {
+
+	public void print() {
+		System.out.println("测试输出！");
+	}
+
+}
