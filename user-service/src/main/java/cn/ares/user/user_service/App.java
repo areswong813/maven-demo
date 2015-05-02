834cfdd5e639036d121d00f8da09cfbcd1ea98f4
ares <areswong813@foxmail.com>
2015-05-03 00:56:26 +0800

    修改测试

--- user-service/src/main/java/cn/ares/user/user_service/App.java
+++ user-service/src/main/java/cn/ares/user/user_service/App.java
@@ -1,8 +1,5 @@
 package cn.ares.user.user_service;
 
-import cn.ares.user.user_core.IUserService;
-import cn.ares.user.user_core.UserService;
-
 /**
  * Hello world!
  *
@@ -11,7 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-    	IUserService u = new UserService();
-    	u.print();
+    	System.out.println("测试");
     }
 }
